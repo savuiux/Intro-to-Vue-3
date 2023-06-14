b98d507bb6c9c030563795ecd40c88248f4f41fa
gabcbiz <gabc.biz@gmail.com>
2023-06-14 14:31:30 +0800

    Lesson 2

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Dress',
+            description: 'This dress is a sophisticated outfit that fits Small to Medium frames.'
+        }
+    }
+});
